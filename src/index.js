3a247e5b099da300f0008bd684a7b46e429f091b
koreteau <77269428+koreteau@users.noreply.github.com>
2024-06-24 16:47:49 +0200

    env file config

--- src/index.js
+++ src/index.js
@@ -2,27 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
 import { ThemeProvider } from "@material-tailwind/react";
-
-
 import firebase from 'firebase/compat/app';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB75X9jHzVEhgXjjwMmAbxsePvS7Nj4lwI",
-  authDomain: "pixi-e10e8.firebaseapp.com",
-  projectId: "pixi-e10e8",
-  storageBucket: "pixi-e10e8.appspot.com",
-  messagingSenderId: "904786726063",
-  appId: "1:904786726063:web:d95501060e60aa9652185a",
-  measurementId: "G-877940Z0DV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
